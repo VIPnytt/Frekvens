1f2600f86fc01e8aedce405ed6f48f84461a272a
Jan-Petter Gundersen <europe.jpg@gmail.com>
2025-08-08 15:52:31 +0200

    Refactor WebSocket to send messages directly to the client on connect instead of broadcasting to all

--- firmware/src/extensions/WebSocketExtension.cpp
+++ firmware/src/extensions/WebSocketExtension.cpp
@@ -15,26 +15,13 @@ WebSocketExtension::WebSocketExtension() : ExtensionModule("WebSocket")
 
 void WebSocketExtension::ready()
 {
-    server->onEvent(&onEvent);
-    WebServer.http->addHandler(server);
+    ws->onEvent(&onEvent);
+    WebServer.http->addHandler(ws);
 }
 
 void WebSocketExtension::handle()
 {
-    if (pending)
-    {
-        pending = false;
-        const JsonDocument doc = Device.getTransmits();
-        const size_t length = measureJson(doc);
-        char *payload = new char[length + 1];
-        serializeJson(doc, payload, length + 1);
-        server->textAll(payload);
-        delete[] payload;
-    }
-    else
-    {
-        server->cleanupClients();
-    }
+    ws->cleanupClients();
 }
 
 void WebSocketExtension::transmitterHook(const JsonDocument &doc, const char *const source)
@@ -44,37 +31,50 @@ void WebSocketExtension::transmitterHook(const JsonDocument &doc, const char *co
     const size_t length = measureJson(_doc);
     char *payload = new char[length + 1];
     serializeJson(_doc, payload, length + 1);
-    server->textAll(payload);
+    ws->textAll(payload, length);
     delete[] payload;
 }
 
 void WebSocketExtension::onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
 {
-    AwsFrameInfo *info = (AwsFrameInfo *)arg;
-    if (type == AwsEventType::WS_EVT_DATA && info->opcode == AwsFrameType::WS_TEXT)
+    switch (type)
     {
+    case AwsEventType::WS_EVT_CONNECT:
+    {
+        const JsonDocument doc = Device.getTransmits();
+        const size_t length = measureJson(doc);
+        char *payload = new char[length + 1];
+        serializeJson(doc, payload, length + 1);
+        client->text(payload, length);
+        delete[] payload;
+    }
+    break;
+    case AwsEventType::WS_EVT_DATA:
+    {
+        AwsFrameInfo *info = (AwsFrameInfo *)arg;
+        if (info->opcode == AwsFrameType::WS_TEXT)
+        {
 #ifdef F_DEBUG
-        if (len < info->len)
-        {
-                Serial.printf("%s: chunked messages is currently not supported\n", WebSocket->name);
-            return;
-        }
-#endif
-        JsonDocument doc;
-        if (!deserializeJson(doc, data, len) && doc.is<JsonObjectConst>())
-        {
-            for (const JsonPairConst pair : doc.as<JsonObjectConst>())
+            if (len < info->len)
             {
-                if (pair.value().is<JsonObjectConst>())
+                Serial.printf("%s: chunked messages is currently not supported\n", WebSocket->name);
+                return;
+            }
+#endif
+            JsonDocument doc;
+            if (!deserializeJson(doc, data, len) && doc.is<JsonObjectConst>())
+            {
+                for (const JsonPairConst pair : doc.as<JsonObjectConst>())
                 {
-                    Device.receive(pair.value(), WebSocket->name, pair.key().c_str());
+                    if (pair.value().is<JsonObjectConst>())
+                    {
+                        Device.receive(pair.value(), WebSocket->name, pair.key().c_str());
+                    }
                 }
             }
         }
     }
-    else if (type == AwsEventType::WS_EVT_CONNECT)
-    {
-        WebSocket->pending = true;
+    break;
     }
 }
 
