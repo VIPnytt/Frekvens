45828b9bbbbaa76915e6c1174daa1a414339e34c
Jan-Petter Gundersen <europe.jpg@gmail.com>
2025-08-22 16:22:34 +0200

    Optimizations for Circle Mode

--- firmware/include/modes/CircleMode.h
+++ firmware/include/modes/CircleMode.h
@@ -6,7 +6,7 @@
 class CircleMode : public ModeModule
 {
 private:
-    const uint8_t maxRadius = round(max(COLUMNS * CELL_WIDTH / (float)CELL_HEIGHT, ROWS / (float)CELL_WIDTH * CELL_HEIGHT) / sqrt(2));
+    const uint8_t maxRadius = round(max(COLUMNS * CELL_WIDTH / (float)CELL_HEIGHT, ROWS / (float)CELL_WIDTH * CELL_HEIGHT) / M_SQRT2);
 
     bool lit = true;
 
@@ -14,16 +14,16 @@ private:
 
     unsigned long lastMillis = 0;
 
-#if (COLUMNS % 2 == 0)
-    const float x = COLUMNS / 2.0 - .5;
+#if COLUMNS % 2
+    static constexpr float x = COLUMNS / 2.0;
 #else
-    const float x = COLUMNS / 2.0;
-#endif
-#if (ROWS % 2 == 0)
-    const float y = ROWS / 2.0 - .5;
+    static constexpr float x = COLUMNS / 2.0 - .5;
+#endif // COLUMNS % 2
+#if ROWS % 2
+    static constexpr float y = ROWS / 2.0;
 #else
-    const float y = ROWS / 2.0;
-#endif
+    static constexpr float y = ROWS / 2.0 - .5;
+#endif // ROWS % 2
 
 public:
     CircleMode() : ModeModule("Circle") {};
