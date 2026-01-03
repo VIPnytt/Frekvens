8266512d4ebc1b52e677efc8f54799b3c67a74f3
Jan-Petter Gundersen <europe.jpg@gmail.com>
2026-01-03 16:56:17 +0100

    Clock refactoring

---
+++ webapp/src/modes/BinaryClock.tsx
@@ -0,0 +1,12 @@
+import { Component } from 'solid-js';
+
+import { ClockIcon } from '../components/Clock';
+import { MainComponent as ModesMainComponent } from '../services/Modes';
+
+export const name = 'Binary clock';
+
+export const Main: Component = () => (
+    <ModesMainComponent
+        icon={ClockIcon()}
+    />
+);
