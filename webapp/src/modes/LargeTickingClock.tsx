02e894c996be373b13853d015368b165188dd7f6
Solmath <33658856+Solmath@users.noreply.github.com>
2026-01-06 21:58:50 +0100

    feat(smaltickingclock): Add checkbox to toggle ticking

--- webapp/src/modes/LargeTickingClock.tsx
+++ webapp/src/modes/LargeTickingClock.tsx
@@ -1,52 +1,49 @@
-import { Component, createSignal } from 'solid-js';
+import { type Component, createSignal } from "solid-js";
 
-import { ClockIcon } from '../components/Clock';
-import { Toast } from '../components/Toast';
-import { Tooltip } from '../components/Tooltip';
-import { MODE_LARGETICKINGCLOCK } from '../config/modules';
-import { SidebarSection } from '../extensions/WebApp';
-import { WebSocketWS } from '../extensions/WebSocket';
-import { name as LargeTickingClockName } from './LargeTickingClock';
-import { MainComponent as ModesMainComponent } from '../services/Modes';
+import { ClockIcon } from "../components/Clock";
+import { Toast } from "../components/Toast";
+import { Tooltip } from "../components/Tooltip";
+import { MODE_LARGETICKINGCLOCK } from "../config/modules";
+import { SidebarSection } from "../extensions/WebApp";
+import { WebSocketWS } from "../extensions/WebSocket";
+import { name as LargeTickingClockName } from "./LargeTickingClock";
+import { MainComponent as ModesMainComponent } from "../services/Modes";
 
-export const name = 'Large ticking clock';
+export const name = "Large ticking clock";
 
 const [getTicking, setTicking] = createSignal<boolean>(true);
 
 const { toast } = Toast();
 
-export const Main: Component = () => (
-  <ModesMainComponent
-    icon={ClockIcon()}
-  />
-);
+export const Main: Component = () => <ModesMainComponent icon={ClockIcon()} />;
 
 export const Sidebar: Component = () => {
-  const handleTicking = (ticking: boolean) => {
-    setTicking(ticking);
-    WebSocketWS.send(JSON.stringify({
-      [name]:
-      {
-        'ticking': getTicking(),
-      },
-    }));
-    toast(`${name} updated`);
-  };
+	const handleTicking = (ticking: boolean) => {
+		setTicking(ticking);
+		WebSocketWS.send(
+			JSON.stringify({
+				[name]: {
+					ticking: getTicking(),
+				},
+			}),
+		);
+		toast(`${name} updated`);
+	};
 
-  return (
-    MODE_LARGETICKINGCLOCK && (
-      <SidebarSection title={name}>
-        <div>
-          Ticking
-          <Tooltip text={LargeTickingClockName}>
-            <input
-              type="checkbox"
-              checked={getTicking()}
-              onChange={(e) => handleTicking(e.currentTarget.checked)}
-            />
-          </Tooltip>
-        </div>
-      </SidebarSection>
-    )
-  );
+	return (
+		MODE_LARGETICKINGCLOCK && (
+			<SidebarSection title={name}>
+				<div>
+					Ticking
+					<Tooltip text={LargeTickingClockName}>
+						<input
+							type="checkbox"
+							checked={getTicking()}
+							onChange={(e) => handleTicking(e.currentTarget.checked)}
+						/>
+					</Tooltip>
+				</div>
+			</SidebarSection>
+		)
+	);
 };
--- webapp/src/modes/SmallTickingClock.tsx
+++ webapp/src/modes/SmallTickingClock.tsx
@@ -1,12 +1,49 @@
-import { Component } from 'solid-js';
+import { type Component, createSignal } from "solid-js";
 
-import { ClockIcon } from '../components/Clock';
-import { MainComponent as ModesMainComponent } from '../services/Modes';
+import { ClockIcon } from "../components/Clock";
+import { Toast } from "../components/Toast";
+import { Tooltip } from "../components/Tooltip";
+import { MODE_SMALLTICKINGCLOCK } from "../config/modules";
+import { SidebarSection } from "../extensions/WebApp";
+import { WebSocketWS } from "../extensions/WebSocket";
+import { name as SmallTickingClockName } from "./LargeTickingClock";
+import { MainComponent as ModesMainComponent } from "../services/Modes";
 
-export const name = 'Small ticking clock';
+export const name = "Small ticking clock";
 
-export const Main: Component = () => (
-    <ModesMainComponent
-        icon={ClockIcon()}
-    />
-);
+const [getTicking, setTicking] = createSignal<boolean>(true);
+
+const { toast } = Toast();
+
+export const Main: Component = () => <ModesMainComponent icon={ClockIcon()} />;
+
+export const Sidebar: Component = () => {
+	const handleTicking = (ticking: boolean) => {
+		setTicking(ticking);
+		WebSocketWS.send(
+			JSON.stringify({
+				[name]: {
+					ticking: getTicking(),
+				},
+			}),
+		);
+		toast(`${name} updated`);
+	};
+
+	return (
+		MODE_SMALLTICKINGCLOCK && (
+			<SidebarSection title={name}>
+				<div>
+					Ticking
+					<Tooltip text={SmallTickingClockName}>
+						<input
+							type="checkbox"
+							checked={getTicking()}
+							onChange={(e) => handleTicking(e.currentTarget.checked)}
+						/>
+					</Tooltip>
+				</div>
+			</SidebarSection>
+		)
+	);
+};
