2c0b5036b0cbffbf373b938b62e5afc4a84ea1dd
Jan-Petter Gundersen <europe.jpg@gmail.com>
2026-05-22 04:25:43 +0200

    Refactored bitmaps and drawing method

--- firmware/include/handlers/ClockHandler.h
+++ firmware/include/handlers/ClockHandler.h
@@ -8,78 +8,18 @@
 class ClockHandler
 {
 private:
-    static constexpr std::array<std::array<uint8_t, 5U>, 10U> digits{{
-        {
-            0b111U,
-            0b101U,
-            0b101U,
-            0b101U,
-            0b111U,
-        },
-        {
-            0b010U,
-            0b110U,
-            0b010U,
-            0b010U,
-            0b111U,
-        },
-        {
-            0b111U,
-            0b001U,
-            0b111U,
-            0b100U,
-            0b111U,
-        },
-        {
-            0b111U,
-            0b001U,
-            0b111U,
-            0b001U,
-            0b111U,
-        },
-        {
-            0b101U,
-            0b101U,
-            0b111U,
-            0b001U,
-            0b001U,
-        },
-        {
-            0b111U,
-            0b100U,
-            0b111U,
-            0b001U,
-            0b111U,
-        },
-        {
-            0b111U,
-            0b100U,
-            0b111U,
-            0b101U,
-            0b111U,
-        },
-        {
-            0b111U,
-            0b001U,
-            0b001U,
-            0b001U,
-            0b001U,
-        },
-        {
-            0b111U,
-            0b101U,
-            0b111U,
-            0b101U,
-            0b111U,
-        },
-        {
-            0b111U,
-            0b101U,
-            0b111U,
-            0b001U,
-            0b111U,
-        },
-    }};
+    static constexpr std::array<uint16_t, 10U> digits{
+        0b111101101101111U,
+        0b010110010010111U,
+        0b111001111100111U,
+        0b111001111001111U,
+        0b101101111001001U,
+        0b111100111001111U,
+        0b111100111101111U,
+        0b111001001001001U,
+        0b111101111101111U,
+        0b111101111001111U,
+    };
 
     const bool opaque{true}; // NOLINT(cppcoreguidelines-avoid-const-or-ref-data-members)
 
@@ -88,7 +28,7 @@ private:
 
     tm local{};
 
-    void draw(const std::array<uint8_t, 5U> &digit, uint8_t x, uint8_t y) const;
+    void draw(uint16_t bitmap, uint8_t x, uint8_t y) const;
 
 public:
     explicit ClockHandler(bool opaque = true) : opaque(opaque) {};
--- firmware/src/handlers/ClockHandler.cpp
+++ firmware/src/handlers/ClockHandler.cpp
@@ -12,26 +12,26 @@ void ClockHandler::handle()
 #if CLOCK_12H
         const uint8_t hour{static_cast<uint8_t>((local.tm_hour + 11) % 12 + 1)};
 #endif // CLOCK_12H
-        draw(digits[hour / 10], GRID_COLUMNS / 2U - 8U, 0U);
-        draw(digits[hour % 10], GRID_COLUMNS / 2U - 4U, 0U);
-        draw(digits[minute / 10], GRID_COLUMNS / 2U + 1U, 0U);
-        draw(digits[minute % 10], GRID_COLUMNS / 2U + 5U, 0U);
+        draw(digits[hour / 10], (GRID_COLUMNS / 2U) - 8U, 0U);
+        draw(digits[hour % 10], (GRID_COLUMNS / 2U) - 4U, 0U);
+        draw(digits[minute / 10], (GRID_COLUMNS / 2U) + 1U, 0U);
+        draw(digits[minute % 10], (GRID_COLUMNS / 2U) + 5U, 0U);
     }
 }
 
-void ClockHandler::draw(const std::array<uint8_t, 5U> &bitmap, uint8_t x, uint8_t y) const
+void ClockHandler::draw(const uint16_t bitmap, uint8_t x, uint8_t y) const
 {
-    for (uint8_t _x{x}; _x < x + 3U; ++_x)
+    for (uint8_t _x{0U}; _x < 3U; ++_x)
     {
-        for (uint8_t _y{y}; _y < y + 5U; ++_y)
+        for (uint8_t _y{0U}; _y < 5U; ++_y)
         {
-            if ((bitmap[_y - y] >> (x + 2U - _x)) & 1U)
+            if ((bitmap >> (14U - (_y * 3U + _x))) & 1U)
             {
-                Display.setPixel(_x, _y);
+                Display.setPixel(static_cast<uint8_t>(x + _x), static_cast<uint8_t>(y + _y));
             }
             else if (opaque)
             {
-                Display.setPixel(_x, _y, 0U);
+                Display.setPixel(static_cast<uint8_t>(x + _x), static_cast<uint8_t>(y + _y), 0U);
             }
         }
     }
@@ -41,17 +41,17 @@ void ClockHandler::clear()
 {
     for (uint8_t y{0U}; y < 5U; ++y)
     {
-        Display.setPixel(GRID_COLUMNS / 2U - 8U, y, 0U);
-        Display.setPixel(GRID_COLUMNS / 2U - 7U, y, 0U);
-        Display.setPixel(GRID_COLUMNS / 2U - 6U, y, 0U);
-        Display.setPixel(GRID_COLUMNS / 2U - 4U, y, 0U);
-        Display.setPixel(GRID_COLUMNS / 2U - 3U, y, 0U);
-        Display.setPixel(GRID_COLUMNS / 2U - 2U, y, 0U);
-        Display.setPixel(GRID_COLUMNS / 2U + 1U, y, 0U);
-        Display.setPixel(GRID_COLUMNS / 2U + 2U, y, 0U);
-        Display.setPixel(GRID_COLUMNS / 2U + 3U, y, 0U);
-        Display.setPixel(GRID_COLUMNS / 2U + 5U, y, 0U);
-        Display.setPixel(GRID_COLUMNS / 2U + 6U, y, 0U);
-        Display.setPixel(GRID_COLUMNS / 2U + 7U, y, 0U);
+        Display.setPixel((GRID_COLUMNS / 2U) - 8U, y, 0U);
+        Display.setPixel((GRID_COLUMNS / 2U) - 7U, y, 0U);
+        Display.setPixel((GRID_COLUMNS / 2U) - 6U, y, 0U);
+        Display.setPixel((GRID_COLUMNS / 2U) - 4U, y, 0U);
+        Display.setPixel((GRID_COLUMNS / 2U) - 3U, y, 0U);
+        Display.setPixel((GRID_COLUMNS / 2U) - 2U, y, 0U);
+        Display.setPixel((GRID_COLUMNS / 2U) + 1U, y, 0U);
+        Display.setPixel((GRID_COLUMNS / 2U) + 2U, y, 0U);
+        Display.setPixel((GRID_COLUMNS / 2U) + 3U, y, 0U);
+        Display.setPixel((GRID_COLUMNS / 2U) + 5U, y, 0U);
+        Display.setPixel((GRID_COLUMNS / 2U) + 6U, y, 0U);
+        Display.setPixel((GRID_COLUMNS / 2U) + 7U, y, 0U);
     }
 }
