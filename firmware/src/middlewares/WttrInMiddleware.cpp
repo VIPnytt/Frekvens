24e3ea489313665f767ef715faa43e9b011f9850
Jan-Petter Gundersen <europe.jpg@gmail.com>
2026-06-09 01:28:10 +0200

    Kelvin for Wttr.in

--- firmware/src/middlewares/WttrInMiddleware.cpp
+++ firmware/src/middlewares/WttrInMiddleware.cpp
@@ -15,43 +15,45 @@ void WttrInMiddleware::update(std::optional<WeatherHandler::Conditions> &conditi
     path = parts.back().first;
     query = parts.back().second;
     std::vector<char> body;
-    const int status = fetch(body, lastMillis);
+    const int status{fetch(body, lastMillis)};
     if (status != 200)
     {
         if (status >= 400 && status < 500)
         {
             parts.pop_back();
-            lastMillis = millis() - interval + (1U << 12U);
+            lastMillis = millis() - interval + (0b1U << 12U);
         }
         return;
     }
     JsonDocument filter; // NOLINT(misc-const-correctness)
 #if TEMPERATURE_FAHRENHEIT
-    filter["current_condition"][0]["temp_F"].set(true);
+    filter["current_condition"][0U]["temp_F"].set(true);
 #else
-    filter["current_condition"][0]["temp_C"].set(true);
+    filter["current_condition"][0U]["temp_C"].set(true);
 #endif // TEMPERATURE_FAHRENHEIT
-    filter["current_condition"][0]["weatherCode"].set(true);
+    filter["current_condition"][0U]["weatherCode"].set(true);
     JsonDocument doc; // NOLINT(misc-const-correctness)
     if (deserializeJson(doc, body.data(), DeserializationOption::Filter(filter)) == DeserializationError::Code::Ok &&
 #if TEMPERATURE_FAHRENHEIT
-        doc["current_condition"][0]["temp_F"].is<std::string_view>() &&
+        doc["current_condition"][0U]["temp_F"].is<std::string_view>() &&
 #else
-        doc["current_condition"][0]["temp_C"].is<std::string_view>() &&
+        doc["current_condition"][0U]["temp_C"].is<std::string_view>() &&
 #endif // TEMPERATURE_FAHRENHEIT
-        doc["current_condition"][0]["weatherCode"].is<std::string_view>())
+        doc["current_condition"][0U]["weatherCode"].is<std::string_view>())
     {
-        condition = getCondition(doc["current_condition"][0]["weatherCode"].as<uint16_t>(), codesets);
+        condition = getCondition(doc["current_condition"][0U]["weatherCode"].as<uint16_t>(), codesets);
 #if TEMPERATURE_FAHRENHEIT
-        temperature = doc["current_condition"][0]["temp_F"].as<int16_t>();
+        temperature = doc["current_condition"][0U]["temp_F"].as<int16_t>();
+#elif TEMPERATURE_KELVIN
+        temperature = static_cast<int16_t>(273 + doc["current_condition"][0U]["temp_C"].as<int>());
 #else
-        temperature = doc["current_condition"][0]["temp_C"].as<int16_t>();
+        temperature = doc["current_condition"][0U]["temp_C"].as<int16_t>();
 #endif // TEMPERATURE_FAHRENHEIT
         return;
     }
     parts.pop_back();
     ESP_LOGD("Response", "unsupported format"); // NOLINT(cppcoreguidelines-pro-type-vararg,hicpp-vararg)
-    lastMillis = millis() - interval + (1U << 13U);
+    lastMillis = millis() - interval + (0b1U << 13U);
 }
 
 #endif // WEATHER_WTTRIN
