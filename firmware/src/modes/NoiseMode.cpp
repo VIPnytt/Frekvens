41bcaa4dd9e31a050845e75cb78395b1e7be5f6b
Jan-Petter Gundersen <europe.jpg@gmail.com>
2026-06-09 03:57:37 +0200

    Noise mode microphone support (#640)
    * Microphone support
    
    * Nolint comment to false positive

--- firmware/src/modes/NoiseMode.cpp
+++ firmware/src/modes/NoiseMode.cpp
@@ -2,7 +2,11 @@
 
 #include "modes/NoiseMode.h"
 
-#include "services/DisplayService.h" // NOLINT(misc-include-cleaner)
+#include "services/DisplayService.h"    // NOLINT(misc-include-cleaner)
+#include "services/ExtensionsService.h" // NOLINT(misc-include-cleaner)
+
+static_assert(GRID_COLUMNS * GRID_ROWS >= 9U,
+              __STRING(MODE_NOISE) " is not compatible with this device's display size.");
 
 void NoiseMode::handle()
 {
@@ -10,10 +14,17 @@ void NoiseMode::handle()
     {
         if (millis() - dot.lastMillis > dot.delay)
         {
-            Display.setPixel(dot.x, dot.y, 0);
+#if EXTENSION_MICROPHONE
+            if (!Extensions.Microphone().isTriggered())
+            {
+                dot.lastMillis = millis();
+                break;
+            }
+#endif // EXTENSION_MICROPHONE
+            Display.setPixel(dot.x, dot.y, 0U);
             dot.x = random(GRID_COLUMNS);
             dot.y = random(GRID_ROWS);
-            dot.delay = random(1U << 6U, INT8_MAX);
+            dot.delay = random(0b1U << 6U, INT8_MAX);
             Display.setPixel(dot.x, dot.y);
             dot.lastMillis = millis();
         }
