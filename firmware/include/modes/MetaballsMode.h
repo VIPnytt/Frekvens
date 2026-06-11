17d07ccdef9350eaa2ede44d3861deff2eba2eb9
Jan-Petter Gundersen <europe.jpg@gmail.com>
2026-06-11 23:09:44 +0200

    Refactor MetaballsMode calculations and improve readability (#649)

--- firmware/include/modes/MetaballsMode.h
+++ firmware/include/modes/MetaballsMode.h
@@ -10,13 +10,13 @@
 class MetaballsMode final : public ModeModule
 {
 private:
-    static constexpr float radius = min<float>(GRID_COLUMNS * PITCH_HORIZONTAL / static_cast<float>(PITCH_VERTICAL),
-                                               GRID_ROWS *PITCH_VERTICAL / static_cast<float>(PITCH_HORIZONTAL)) /
-                                    std::numbers::pi;
-    static constexpr float radiusSq = radius * radius;
-    static constexpr float speed = 5e-6F * static_cast<float>(GRID_COLUMNS * GRID_ROWS);
+    static constexpr float radius{min<float>(GRID_COLUMNS * PITCH_HORIZONTAL / static_cast<float>(PITCH_VERTICAL),
+                                             GRID_ROWS *PITCH_VERTICAL / static_cast<float>(PITCH_HORIZONTAL)) /
+                                  5.0F};
+    static constexpr float radiusSq{radius * radius};
+    static constexpr float speed{1e-6F * static_cast<float>(GRID_COLUMNS * GRID_ROWS)};
 
-    static constexpr uint8_t multiplier = 1U << 3U;
+    static constexpr uint8_t multiplier{1U << 4U};
 
     struct Ball
     {
@@ -27,7 +27,7 @@ private:
     };
 
     std::array<uint8_t, GRID_COLUMNS * GRID_ROWS> contributions{};
-    std::array<Ball, GRID_COLUMNS * GRID_ROWS / (1U << 6U)> balls{};
+    std::array<Ball, GRID_COLUMNS * GRID_ROWS / 50U> balls{};
 
 public:
     static constexpr std::string_view name{"Metaballs"};
