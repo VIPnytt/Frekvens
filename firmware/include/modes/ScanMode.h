44cbe7fadddc53f55bcfc0cafd47e69ad26e0726
Jan-Petter Gundersen <europe.jpg@gmail.com>
2026-06-19 17:45:30 +0200

    Fixes an issue with the Scan mode where the scaner would lit up but doesn't move over the screen as it should

--- firmware/include/modes/ScanMode.h
+++ firmware/include/modes/ScanMode.h
@@ -7,9 +7,9 @@
 class ScanMode final : public ModeModule
 {
 private:
-    uint8_t column = 0;
+    uint8_t column{0U};
 
-    unsigned long lastMillis = 0;
+    unsigned long lastMillis{0UL};
 
 public:
     static constexpr std::string_view name{"Scan"};
