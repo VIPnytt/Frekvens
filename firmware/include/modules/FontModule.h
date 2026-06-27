c90c72d767f6c7330ee8f2217690f3bb0ce6a2e5
Jan-Petter Gundersen <europe.jpg@gmail.com>
2026-06-27 19:08:12 +0200

    Refactor BitmapHandler and FontModule to inline implementations (#685)
    * Refactor BitmapHandler and FontModule: inline template implementations and remove .tpp files
    
    * Add missing headers + remove implicit integer-to-bool conversion
    
    * Formatting and nolint comment

--- firmware/include/modules/FontModule.h
+++ firmware/include/modules/FontModule.h
@@ -37,4 +37,21 @@ protected:
     [[nodiscard]] Symbol whitespace(uint8_t offsetX) const;
 };
 
-#include "modules/FontModule.tpp"
+template <typename T, size_t N> FontModule::Symbol FontModule::toSymbol(const std::array<T, N> &bitmap) const
+{
+    return {bitmap, 0U, 0};
+}
+
+template <typename T, size_t N>
+// NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
+FontModule::Symbol FontModule::toSymbol(const std::array<T, N> &bitmap, int8_t offsetY) const
+{
+    return {bitmap, 0U, offsetY};
+}
+
+template <typename T, size_t N>
+// NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
+FontModule::Symbol FontModule::toSymbol(const std::array<T, N> &bitmap, uint8_t offsetX, int8_t offsetY) const
+{
+    return {bitmap, offsetX, offsetY};
+}
