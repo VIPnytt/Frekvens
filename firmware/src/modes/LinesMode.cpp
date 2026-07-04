8837f4d106321d72381f9b2831628d5c7ee4db70
Jan-Petter Gundersen <europe.jpg@gmail.com>
2026-07-04 03:59:32 +0200

    Optimize calculations and change variable initialization styling (#703)

--- firmware/src/modes/LinesMode.cpp
+++ firmware/src/modes/LinesMode.cpp
@@ -3,9 +3,10 @@
 #include "modes/LinesMode.h"
 
 #include "config/constants.h" // NOLINT(misc-include-cleaner)
-#include "extensions/MicrophoneExtension.h"
 #include "services/DisplayService.h"
 #include "services/ExtensionsService.h"
+
+static_assert(GRID_COLUMNS >= 4U, __STRING(MODE_LINES) " is not compatible with this device's display size.");
 
 void LinesMode::handle()
 {
@@ -16,27 +17,27 @@ void LinesMode::handle()
 #endif // EXTENSION_MICROPHONE
     {
         lastMillis = millis();
-        for (uint8_t offset = 0; offset < GRID_COLUMNS + 2; offset += 4)
+        for (uint8_t offset{0U}; offset < GRID_COLUMNS + 2U; offset += 4U)
         {
             if (x + offset < GRID_COLUMNS)
             {
-                for (uint8_t y = 0; y < GRID_ROWS; ++y)
+                for (uint8_t y{0U}; y < GRID_ROWS; ++y)
                 {
-                    Display.setPixel(x + offset, y);
+                    Display.setPixel(x + offset, y, UINT8_MAX);
                 }
             }
-            if (x + offset - 2 >= 0 && x + offset - 2 < GRID_COLUMNS)
+            if (x + offset >= 2U && x + offset < GRID_COLUMNS + 2U)
             {
-                for (uint8_t y = 0; y < GRID_ROWS; ++y)
+                for (uint8_t y{0U}; y < GRID_ROWS; ++y)
                 {
-                    Display.setPixel(x + offset - 2, y, 0);
+                    Display.setPixel(x + offset - 2U, y, 0U);
                 }
             }
         }
         ++x;
-        if (x >= 4)
+        if (x >= 4U)
         {
-            x = 0;
+            x = 0U;
         }
     }
 }
